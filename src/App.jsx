ee4e29c53f59d987525f19c2e089d4a7cbe7ce0d
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-14 14:58:11 +0530

    Navigation Bar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavigationBar from './components/NavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
-      
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NavigationBar />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
